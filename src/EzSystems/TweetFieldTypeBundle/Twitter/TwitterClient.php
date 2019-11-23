9236eba86a8658c8a230acf3667bd360852e3b8b
Unknown <rhidja@gmail.com>
2019-11-23 14:35:59 +0100

    Corriger le bug de l'autowiring

--- src/EzSystems/TweetFieldTypeBundle/Twitter/TwitterClient.php
+++ src/EzSystems/TweetFieldTypeBundle/Twitter/TwitterClient.php
@@ -6,6 +6,7 @@
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
 namespace EzSystems\TweetFieldTypeBundle\Twitter;
+
 class TwitterClient implements TwitterClientInterface
 {
     public function getEmbed($statusUrl)
