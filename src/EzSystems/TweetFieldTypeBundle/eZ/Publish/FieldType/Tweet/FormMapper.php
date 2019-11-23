6000564dc2c97e030bc7efe22b7f80b1a7385012
Unknown <rhidja@gmail.com>
2019-11-23 14:52:29 +0100

    Step 7 - Allow adding and editing the Field in Back Office

---
+++ src/EzSystems/TweetFieldTypeBundle/eZ/Publish/FieldType/Tweet/FormMapper.php
@@ -0,0 +1,61 @@
+<?php
+namespace EzSystems\TweetFieldTypeBundle\eZ\Publish\FieldType\Tweet;
+
+use EzSystems\RepositoryForms\FieldType\FieldValueFormMapperInterface;
+use EzSystems\TweetFieldTypeBundle\Form\TweetValueTransformer;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormInterface;
+use EzSystems\RepositoryForms\Data\Content\FieldData;
+use eZ\Publish\Core\Repository\FieldTypeService;
+
+
+/**
+ * Class FormMapper
+ * @package EzSystems\TweetFieldTypeBundle\eZ\Publish\FieldType\Tweet
+ */
+class FormMapper implements FieldValueFormMapperInterface
+{
+    /**
+     * @var FieldTypeService
+     */
+    private $fieldTypeService;
+
+    /**
+     * FormMapper constructor.
+     * @param FieldTypeService $fieldTypeService
+     */
+    public function __construct(FieldTypeService $fieldTypeService)
+    {
+        $this->fieldTypeService = $fieldTypeService;
+    }
+
+    /**
+     * @param FormInterface $fieldForm
+     * @param FieldData $data
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function mapFieldValueForm(FormInterface $fieldForm, FieldData $data)
+    {
+        $fieldDefinition = $data->fieldDefinition;
+        $formConfig = $fieldForm->getConfig();
+        $names = $fieldDefinition->getNames();
+        $label = $fieldDefinition->getName($formConfig->getOption('mainLanguageCode')) ?: reset($names);
+        $fieldType = $this->fieldTypeService->getFieldType($fieldDefinition->fieldTypeIdentifier);
+        $fieldForm
+            ->add(
+                $formConfig->getFormFactory()->createBuilder()
+                    ->create(
+                        'value',
+                        TextType::class,
+                        [
+                            'required' => false,
+                            'label' => $label
+                        ]
+                    )
+                    // Deactivate auto-initialize as you're not on the root form.
+                    ->setAutoInitialize(false)
+                    ->addModelTransformer(new TweetValueTransformer($fieldType))
+                    ->getForm()
+            );
+    }
+}
